af4a34b404be3c9142671e98a06b1ae2a632f3d3
puerchik <rumyantsevnn@yandex.ru>
2024-03-12 09:52:37 +0300

    basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import s from "./App.module.css";
 function App() {
   return (
     <>
-      <h1 className={s.title}>hello</h1>
+      <div className={s.wrapper}>
+        <div className={s.adviceBox}>
+          <h3 className={s.adviceBoxTitle}>ADVICE #89</h3>
+          <p className={s.adviceBoxText}>
+            “It is easy to sit up and take notice, What is difficult is getting up and
+            taking action.”
+          </p>
+          <button className={s.adviceBoxButton}>adviceBoxButton</button>
+        </div>
+      </div>
       <div className="attribution">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
