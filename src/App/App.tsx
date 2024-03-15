0d4285c97745f51e8ff579a4fae34d26d2c6ef91
puerchik <rumyantsevnn@yandex.ru>
2024-03-15 09:00:41 +0300

    divider is connected in a different way

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
+
 import { appActions } from "../store/adviceGeneratorSlice";
 import { getAdvice } from "../features/api";
-import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
+
+import divider from "./../assets/images/pattern-divider-desktop.svg";
 import s from "./App.module.css";
 
 function App() {
@@ -23,6 +26,7 @@ function App() {
         <div className={s.adviceBox}>
           <h3 className={s.adviceBoxTitle}>{`ADVICE #${advice.id}`}</h3>
           <p className={s.adviceBoxText}>{advice.advice}</p>
+          <img className={s.adviceBoxImg} src={divider} alt="divider" />
           <button onClick={setAdvice} className={s.adviceBoxButton}></button>
         </div>
       </div>
